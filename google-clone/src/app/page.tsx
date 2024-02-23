e8520df72f5abf2cdd5efb6cf682528e726584a0
Nhung Nguyen <nhungntc@smartosc.com>
2024-02-23 16:13:18 +0700

    Add home header component

---
+++ google-clone/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { HomeHeader } from "@/components/HomeHeader";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
+}
---
+++ google-clone/src/components/HomeHeader.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { TbGridDots } from "react-icons/tb";
+
+export interface Props {}
+
+export function HomeHeader(props: Props) {
+  return (
+    <header className="flex justify-end p-5 text-sm">
+      <div className="flex space-x-4 items-center">
+        <Link className="hover:underline" href={"https://gmail.com"}>
+          Gmail
+        </Link>
+        <Link className="hover:underline" href={"https://image.gmail.com"}>
+          Images
+        </Link>
+        <TbGridDots className="bg-transparent hover: bg-gray-200 rounded-full text-4xl p-2" />
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">
+          Sign in
+        </button>
+      </div>
+    </header>
+  );
+}
