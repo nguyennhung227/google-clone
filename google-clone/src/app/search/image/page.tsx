03e691e6d7d197eaea115a8c9c8e1dcaf1e6315e
Nguyen Nhung <nguyennhung227@gmail.com>
2024-03-04 17:55:23 +0700

    Search by images

--- google-clone/src/app/search/image/page.tsx
+++ google-clone/src/app/search/image/page.tsx
@@ -1,3 +1,36 @@
-export default function ImageSearchPage() {
-  return <div>Image search page</div>;
+import ImageSearchResults from "@/components/ImageSearchResults";
+import Link from "next/link";
+
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function ImageSearchPage({ searchParams }: Props) {
+  const startIndex = searchParams.start || "1";
+
+  const term = searchParams.searchTerm;
+  const response =
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CX}&q=${term}&searchType=image&start=${startIndex}
+`);
+  if (!response.ok) throw new Error("Something went wrong");
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching the web or images for something else{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
 }
